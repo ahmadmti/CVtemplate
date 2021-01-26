8b1ed8d795271ef0c3353ad90c42d02dddc72c67
root <shahid.hussain@geeklone.com>
2021-01-26 13:24:56 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('HomePage');
+    // return view('HomePage');
+    return 'Hello world !!';
 });
 
 // HomePage
